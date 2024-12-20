78d837bbf6740c8bcf7077af9fe9eacd44a2a172
Sven Beisiegel <162966719+svenbeisiegel@users.noreply.github.com>
2024-12-20 15:01:51 +0100

    Update docker-entrypoint.sh (#4)
    * Update docker-entrypoint.sh
    
    * update libicu-devel to 76
    
    * Update build-docker-image.yml
    
    * Update docker-entrypoint.sh
    
    * Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,16 @@
 
 set -e
 
+if [ "$SINGLE_NODE" ]; then
+  printf "[couchdb]\nsingle_node = true\n\n[cluster]\nn = 1\n" > /opt/couchdb/etc/default.d/00-setup.ini
+fi
+
 if [ "$COUCHDB_USER" ] && [ "$COUCHDB_PASSWORD" ]; then
-  printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > /opt/couchdb/etc/local.d/99-creds.ini
+  printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > /opt/couchdb/etc/default.d/99-creds.ini
 fi
 
 if [ "$COUCHDB_SECRET" ]; then
-  printf "\n[chttpd_auth]\nsecret = %s\n" "$COUCHDB_SECRET" > /opt/couchdb/etc/local.d/99-secret.ini
+  printf "[chttpd_auth]\nsecret = %s\n" "$COUCHDB_SECRET" > /opt/couchdb/etc/default.d/99-secret.ini
 fi
 
 if [ "$NODENAME" ]; then
@@ -16,7 +20,7 @@ else
   printf "\n-name couchdb@localhost\n" >> /opt/couchdb/etc/vm.args
 fi
 
-printf "[chttpd]\nbind_address = 0.0.0.0\nport = 5984\n" > /opt/couchdb/etc/local.d/10-binding.ini
-printf "[couchdb]\njs_engine = quickjs\n" > /opt/couchdb/etc/local.d/10-quickjs.ini
+printf "[chttpd]\nbind_address = 0.0.0.0\nport = 5984\n" > /opt/couchdb/etc/default.d/10-binding.ini
+printf "[couchdb]\njs_engine = quickjs\n" > /opt/couchdb/etc/default.d/10-quickjs.ini
 
 exec "$@"
