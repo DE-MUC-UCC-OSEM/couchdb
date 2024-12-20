de0f86b32a85ffe5b34ede4fd242d095d778cfb8
Sven Beisiegel <162966719+svenbeisiegel@users.noreply.github.com>
2024-12-20 12:40:35 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [ "$COUCHDB_USER" ] && [ "$COUCHDB_PASSWORD" ]; then
-  printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > /opt/couchdb/etc/local.d/99-creds.ini
+  printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > /opt/couchdb/etc/default.d/99-creds.ini
 fi
 
 if [ "$COUCHDB_SECRET" ]; then
-  printf "\n[chttpd_auth]\nsecret = %s\n" "$COUCHDB_SECRET" > /opt/couchdb/etc/local.d/99-secret.ini
+  printf "\n[chttpd_auth]\nsecret = %s\n" "$COUCHDB_SECRET" > /opt/couchdb/etc/default.d/99-secret.ini
 fi
 
 if [ "$NODENAME" ]; then
@@ -16,7 +16,7 @@ else
   printf "\n-name couchdb@localhost\n" >> /opt/couchdb/etc/vm.args
 fi
 
-printf "[chttpd]\nbind_address = 0.0.0.0\nport = 5984\n" > /opt/couchdb/etc/local.d/10-binding.ini
-printf "[couchdb]\njs_engine = quickjs\n" > /opt/couchdb/etc/local.d/10-quickjs.ini
+printf "[chttpd]\nbind_address = 0.0.0.0\nport = 5984\n" > /opt/couchdb/etc/default.d/10-binding.ini
+printf "[couchdb]\njs_engine = quickjs\n" > /opt/couchdb/etc/default.d/10-quickjs.ini
 
 exec "$@"
